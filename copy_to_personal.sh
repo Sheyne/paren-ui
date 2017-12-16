83096494c32c74e2f8b28ddf4055b17aa112983c
Sheyne Anderson <sanderson@novacoast.com>
2017-12-15 21:19:55 -0700

    Improved publish script

--- copy_to_personal.sh
+++ copy_to_personal.sh
@@ -3,3 +3,7 @@ sed \$d main.js > ../Sheyne.com/main.js
 sed \$d worker.js > ../Sheyne.com/worker.js
 cp index.html ../Sheyne.com/toy-lang.html
 cp style.css ../Sheyne.com
+cd ../Sheyne.com
+git add .
+git commit
+git push
